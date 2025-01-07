ea866164e51e9d830fc11933e5d11295907c6888
rt-desai <abhyuday.desai@readytensor.com>
2025-01-06 16:18:24 -0800

    update saved file name

--- src/paths.py
+++ src/paths.py
@@ -3,6 +3,5 @@ import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-
 OUTPUT_DIR = os.path.join(ROOT_DIR, "output")
-WORD_CLOUD_FPATH = os.path.join(OUTPUT_DIR, "wordcloud.png")
+WORD_CLOUD_FPATH = os.path.join(OUTPUT_DIR, "wordcloud-square.png")
